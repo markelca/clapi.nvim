d9e59e294f7771f3888cf82e73fabf7f010a4ccc
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-01-29 22:41:05 +0100

    First builtin

--- lua/telescope/_extensions/clapi.lua
+++ lua/telescope/_extensions/clapi.lua
@@ -1,54 +1,87 @@
-local has_telescope, telescope = pcall(require, "telescope")
-
-if not has_telescope then
-	error("This plugin requires nvim-telescope/telescope.nvim")
-end
-
--- Create the Telescope picker
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local conf = require("telescope.config").values
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
--- Your custom picker function
+
+-- This is a picker - it's the basic display mechanism
 local function custom_picker(opts)
 	opts = opts or {}
-
-	local results = {
-		{ value = "Result 1", info = "Additional info 1" },
-		{ value = "Result 2", info = "Additional info 2" },
-	}
-
 	pickers
 		.new(opts, {
-			prompt_title = "Custom Results",
+			prompt_title = "Basic Picker",
 			finder = finders.new_table({
-				results = results,
-				entry_maker = function(entry)
-					return {
-						value = entry,
-						display = entry.value,
-						ordinal = entry.value,
-					}
-				end,
+				results = { "Result 1", "Result 2" },
 			}),
 			sorter = conf.generic_sorter(opts),
-			attach_mappings = function(prompt_bufnr, map)
-				actions.select_default:replace(function()
-					local selection = action_state.get_selected_entry()
-					actions.close(prompt_bufnr)
-					print("Selected: " .. selection.value.value)
-					print("Info: " .. selection.value.info)
-				end)
-				return true
-			end,
 		})
 		:find()
 end
 
--- Register the extension
-return telescope.register_extension({
+-- This is a builtin - it's a more complete feature that might use multiple pickers
+local builtin = function(opts)
+	opts = opts or {}
+
+	-- You might have some setup logic here
+	local results = {}
+
+	-- Maybe determine which type of picker to show based on context
+	if opts.mode == "simple" then
+		results = { "Simple 1", "Simple 2" }
+	else
+		results = {
+			{ name = "Complex 1", type = "feature" },
+			{ name = "Complex 2", type = "bug" },
+		}
+	end
+
+	-- You might have multiple picker configurations
+	local picker_opts = {
+		prompt_title = "Complex Builtin",
+		finder = finders.new_table({
+			results = results,
+			entry_maker = function(entry)
+				if type(entry) == "string" then
+					return {
+						value = entry,
+						display = entry,
+						ordinal = entry,
+					}
+				else
+					return {
+						value = entry,
+						display = entry.name,
+						ordinal = entry.name,
+						type = entry.type,
+					}
+				end
+			end,
+		}),
+		sorter = conf.generic_sorter(opts),
+		attach_mappings = function(prompt_bufnr, map)
+			-- Complex custom actions
+			actions.select_default:replace(function()
+				local selection = action_state.get_selected_entry()
+				actions.close(prompt_bufnr)
+				-- Do something with the selection
+			end)
+			return true
+		end,
+	}
+
+	-- You might add previewer configuration
+	if opts.preview then
+		picker_opts.previewer = conf.file_previewer(opts)
+	end
+
+	-- Finally create the picker with all the configuration
+	return pickers.new(opts, picker_opts):find()
+end
+
+-- Register both as part of your extension
+return require("telescope").register_extension({
 	exports = {
-		custom_picker = custom_picker,
+		custom_picker = custom_picker, -- Simple picker
+		my_builtin = builtin, -- More complex builtin
 	},
 })
