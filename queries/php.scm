5504487f256f44881df0d0047ff76909ec2d5f7c
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-03-09 21:38:38 +0100

    Added class properties to the query

--- queries/php.scm
+++ queries/php.scm
@@ -1,8 +1,13 @@
 (method_declaration
   (visibility_modifier) @visibility
-  name: (name) @method_name
-)
+  name: (name) @method_name)
+
 (property_promotion_parameter
   visibility: (visibility_modifier) @visibility
-  name: (variable_name) @prop_name
-)
+  name: (variable_name) @prop_name)
+
+(property_declaration
+  (visibility_modifier) @visibility
+  (property_element
+    name: (variable_name) @prop_name)) 
+
