db753e03793b160d316d2ba8d0e49860bd29ce1c
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-03-19 20:42:47 +0100

    Removed unused files

--- lua/clapi/parent.lua
+++
@@ -1,5 +0,0 @@
-local parsers = require("nvim-treesitter.parsers")
-
-local M = {}
-
-return M
--- lua/clapi/sandbox.lua
+++
@@ -1,2 +0,0 @@
-local parent = require("clapi.parent")
-local p = parent.get_file_from_position({ bufnr = 45, position = { character = 27, line = 9 } })
