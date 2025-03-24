f49cb6c41c8cf1c57d15e4e61229abd452985931
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-03-24 21:28:40 +0100

    Refactoring tests

--- tests/manual/treesitter_spec.lua
+++ tests/manual/treesitter_spec.lua
@@ -1,10 +1,10 @@
-local ts = require("clapi.treesitter")
+local lsp = require("clapi.lsp")
 local async = require("plenary.async")
 
 async.run(function()
-	local file_path = ts.get_file_from_position({
+	local file_path = lsp.get_file_from_position({
 		position = { line = 9, character = 27 },
-		bufnr = 4,
+		bufnr = 6,
 	})
 	if file_path then
 		print("Found file:", file_path)
