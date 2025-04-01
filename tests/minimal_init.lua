3176d7b959673658e3fe1691537980a9cf4ec8da
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-04-01 19:01:38 +0200

    Add runtimepaths log to debug gh action

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -5,5 +5,6 @@ vim.opt.runtimepath:append("~/.local/share/nvim/lazy/plenary.nvim")
 vim.opt.runtimepath:append("~/.local/share/nvim/lazy/telescope.nvim")
 vim.opt.runtimepath:append("~/.local/share/nvim/lazy/nvim-treesitter")
 
+vim.print(vim.api.nvim_list_runtime_paths())
 -- Load the plugin
 -- require("clapi")
