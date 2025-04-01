36d7f2f476c3ee74a02566c61ff008adf9b01f5a
Markel Cuesta <cuestaarribas.markel@proton.me>
2025-04-01 19:00:12 +0200

    Modified runtimepath with lua + adding current folder too with absolute path

--- tests/minimal_init.lua
+++ tests/minimal_init.lua
@@ -1,9 +1,9 @@
 -- Minimal init for running tests
-vim.cmd([[set runtimepath+=.]])
-vim.cmd([[set runtimepath+=~/.local/share/nvim/lazy/clapi.nvim]])
-vim.cmd([[set runtimepath+=~/.local/share/nvim/lazy/plenary.nvim]])
-vim.cmd([[set runtimepath+=~/.local/share/nvim/lazy/telescope.nvim]])
-vim.cmd([[set runtimepath+=~/.local/share/nvim/lazy/nvim-treesitter]])
+vim.opt.runtimepath:append(vim.fn.getcwd())
+vim.opt.runtimepath:append("~/.local/share/nvim/lazy/clapi.nvim")
+vim.opt.runtimepath:append("~/.local/share/nvim/lazy/plenary.nvim")
+vim.opt.runtimepath:append("~/.local/share/nvim/lazy/telescope.nvim")
+vim.opt.runtimepath:append("~/.local/share/nvim/lazy/nvim-treesitter")
 
 -- Load the plugin
-require("clapi")
+-- require("clapi")
